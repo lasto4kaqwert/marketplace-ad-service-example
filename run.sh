ff70e80b9825f46e9d666da5a0c34ef1a6c16c0e
lasto4kaqwert <lasto4kaqwert@gmail.com>
2026-08-26 00:17:56 +0300

    kafka topic

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ set -e
 
 export DATABASE_URL="postgresql+asyncpg://${POSTGRES_CONNECTION_STRING#postgres://}"
 export KAFKA_BOOTSTRAP_SERVERS="$KAFKA_BROKERS"
+export KAFKA_TOPIC_ADS="student_lasto4kaqwert-marketplace.ads"
 
 uv run alembic upgrade head
-
 uv run python -m bin.outbox &
 exec uv run python -m bin.api
